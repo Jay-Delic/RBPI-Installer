b3e028f47fa4bfb9dad80f65f2a8c237e49d37ee
Jay-Delic <43278504+Jay-Delic@users.noreply.github.com>
2024-08-10 12:49:48 +0200

    Update RBPI_Installer.sh

--- RBPI_Installer.sh
+++ RBPI_Installer.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Install Conky
-apt insstall conky -y
+apt install conky -y
 
-# Download files
-curl https://github.com/Jay-Delic/RBPI-Installer/blob/main/JD_Wallpaper.jpg
+# Mount Folder
+cd ~/RBPI-Installer
 
 # Adjust Wallpaper
-mv ~/JD_Wallpaper.jpg ~/Pictures/JD_Wallpaper.jpg
+mv JD_Wallpaper.jpg ~/Pictures/JD_Wallpaper.jpg
 pcmanfm --set-wallpaper ~/Pictures/JD_Wallpaper.jpg
 
 # Configure and start Conky
-curl https://github.com/Jay-Delic/RBPI-Installer/blob/main/.conkyrc
-curl https://github.com/Jay-Delic/RBPI-Installer/blob/main/conky.desktop
+cp .conkyrc ~/.conkyrc
 
 mkdir ~/.config/autostart
 mv conky.desktop ~/.config/autostart/conky.desktop
